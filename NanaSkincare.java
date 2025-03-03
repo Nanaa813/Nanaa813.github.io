38dc43c5b574494681d43e133038f492a4a97efd
Nanaa813 <alee95514@gmail.com>
2025-03-03 17:22:27 +0800

    pbo

--- NanaSkincare.java
+++ NanaSkincare.java
@@ -1,4 +1,4 @@
-public class Skincare {
+public class NanaSkincare {
     String Produk;
     String Brand;
     String Jenis;
@@ -15,7 +15,18 @@ public class Skincare {
     }
 
     public static void main(String[] args) {
-        Skincare produk1 = new Skincare();
+        // Biodata Mahasiswa
+        String nama = "Andi Nanis Sacharina";
+        String nim = "F1G123018";
+        String kelas = "Genap";
+
+        System.out.println("Nama  : " + nama);
+        System.out.println("NIM   : " + nim);
+        System.out.println("Kelas : " + kelas);
+        System.out.println("==============================");
+
+        // Objek Skincare
+        NanaSkincare produk1 = new NanaSkincare();
         produk1.Produk = "Sunscreen Aqua Rich";
         produk1.Brand = "Biore"; 
         produk1.Jenis = "Sunscreen";
@@ -23,7 +34,7 @@ public class Skincare {
         produk1.KandunganUtama = "Hyaluronic Acid";
         produk1.info();
 
-        Skincare produk2 = new Skincare();
+        NanaSkincare produk2 = new NanaSkincare();
         produk2.Produk = "Hydrating Toner";
         produk2.Brand = "Hada Labo"; 
         produk2.Jenis = "Toner"; 
@@ -31,7 +42,7 @@ public class Skincare {
         produk2.KandunganUtama = "Hyaluronic Acid";
         produk2.info();
 
-        Skincare produk3 = new Skincare();
+        NanaSkincare produk3 = new NanaSkincare();
         produk3.Produk = "Niacinamide Serum"; 
         produk3.Brand = "The Ordinary"; 
         produk3.Jenis = "Serum"; 
@@ -39,12 +50,20 @@ public class Skincare {
         produk3.KandunganUtama = "Niacinamide";
         produk3.info();
 
-        Skincare produk4 = new Skincare();
+        NanaSkincare produk4 = new NanaSkincare();
         produk4.Produk = "Moisturizer Hydro Boost"; 
         produk4.Brand = "Neutrogena"; 
         produk4.Jenis = "Moisturizer"; 
         produk4.Harga = 170000; 
         produk4.KandunganUtama = "Hyaluronic Acid";
         produk4.info();
+
+        NanaSkincare produk5 = new NanaSkincare();
+        produk5.Produk = "Gentle Cleanser"; 
+        produk5.Brand = "Cetaphil"; 
+        produk5.Jenis = "Cleanser"; 
+        produk5.Harga = 125000; 
+        produk5.KandunganUtama = "Glycerin";
+        produk5.info();
     }
 }
